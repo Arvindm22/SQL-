3ceb20665bc3502630853309d109222f6f7a9cfb
Arvind Muralidharan <arvimurali.99@gmail.com>
2023-06-28 12:05:52 +0530

    Completed assignments till the topic of Unions

--- Assignment SQL.sql
+++ Assignment SQL.sql
@@ -76,4 +76,74 @@ FROM payments PY
 JOIN clients c ON 
 		PY.client_id = c.client_id
 JOIN payment_methods PM ON
-			PY.payment_method = PM.payment_method_id
+			PY.payment_method = PM.payment_method_id;
+            
+  -- OUTER JOIN          
+  SELECT p.product_id, name, o.quantity 
+  FROM products p
+  LEFT JOIN order_items o ON 
+			p.product_id = o.product_id;
+
+-- OUTER JOIN MULTIPLE TABLES
+SELECT 	
+	o.order_date,
+    o.order_id,
+    c.first_name,
+    s.name as Shipper,
+    os.name as Status
+    
+FROM orders o
+LEFT JOIN  customers c ON
+			o.customer_id = c.customer_id
+LEFT JOIN shippers s ON
+			o.shipper_id = s.shipper_id
+JOIN order_statuses os ON
+			o.status = os.order_status_id
+ORDER BY os.name;
+
+-- USING CLAUSE
+SELECT 
+		p.date,
+        c.name as Client,
+        p.amount,
+        pym.name as Mode_of_Payment
+FROM payments p
+JOIN clients c
+		USING(client_id)
+JOIN payment_methods pym ON
+			p.payment_method = pym.payment_method_id;
+
+-- CROSS JOIN 
+-- 1.Implicit Syntax
+SELECT *
+FROM shippers, products;
+-- 2.EXPLICIT Syntax
+SELECT *
+FROM shippers
+CROSS JOIN products;
+
+-- UNIONS
+SELECT 
+	c.customer_id,
+    c.first_name as Name,
+    c.points,
+    'BRONZE' as Medal
+FROM customers c
+WHERE points <2000
+UNION
+SELECT 
+	customer_id,
+    first_name as Name,
+    points,
+    'SILVER' as Medal
+FROM customers 
+WHERE points BETWEEN 2000 AND 3000
+UNION
+SELECT 
+	customer_id,
+    first_name as Name,
+    points,
+    'GOLD' as Medal
+FROM customers 
+WHERE points >3000
+ORDER BY Name;
